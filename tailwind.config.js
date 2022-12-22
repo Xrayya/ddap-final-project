2b00cd483f940e16c62ac81ec72255200a35cd9b
Xrayya <nur.alkhiyar@gmail.com>
2022-12-23 06:01:10 +0700

    little update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    fontSize: {
+      sm: ['14px', '24px'],
+      base: ['16px', '24px'],
+      lg: ['18px', '24px'],
+      xl: ['20px', '32px'],
+      '2xl': ['24px', '32px'],
+      '3xl': ['32px', '40px'],
+      '4xl': ['40px', '48px'],
+      '5xl': ['48px', '64px'],
+      '6xl': ['56px', '72px'],
+    },
+    extend: {
+      colors: {},
+    },
   },
   plugins: [],
 }
